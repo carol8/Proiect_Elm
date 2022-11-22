fb4d1eb9a0c8a99f7e210129b3224b3f7aceb593
Cristian-Carol Budiul <cristib_2002@yahoo.com>
2022-11-22 18:00:23 +0200

    Initial commit

---
+++ src/Model/PersonalDetails.elm
@@ -0,0 +1,29 @@
+module Model.PersonalDetails exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (class, classList, id, href)
+
+
+type alias DetailWithName =
+    { name : String
+    , detail : String
+    }
+
+
+type alias PersonalDetails =
+    { name : String
+    , contacts : List DetailWithName
+    , intro : String
+    , socials : List DetailWithName
+    }
+
+
+view : PersonalDetails -> Html msg
+view details =
+    div [] 
+    ([ h1 [id "name"] [text details.name]
+    ,  em [id "intro"] [text details.intro]]
+    ++ (details.contacts |> List.map (\contact -> p [class "contact-detail"][text contact.detail]))
+    ++ (details.socials |> List.map (\socials -> a [href socials.detail, class "social-link"][text socials.name]))
+    )
+    -- Debug.todo "Implement the Model.PersonalDetails.view function"
